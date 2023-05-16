54330c5544948de50c4bb6056d7ba9d5b60fc520
tonmoy-dev <rxdevs100@gmail.com>
2023-05-17 00:25:38 +0600

    update: disease demo plant image added

--- src/components/Detector.jsx
+++ src/components/Detector.jsx
@@ -10,13 +10,18 @@ import { MdFavorite, MdShare, MdExpandMore, MdMoreVert } from "react-icons/md";
 
 export default function Detector() {
   return (
-    <Card sx={{ maxWidth: "auto", padding: "16px" }}>
+    <Card sx={{ maxWidth: "auto", padding: "16px", margin: "auto" }}>
       <CardHeader
         sx={{ textAlign: "center" }}
         title="Potato Disease Detection"
         subheader=""
       />
-      <CardMedia component="img" height="194" image="" alt="Potato Image" />
+      <CardMedia 
+		  component="img" 
+		  height="300" 
+		  image="https://i.ibb.co/LkyR77D/0a6983a5-895e-4e68-9edb-88adf79211e9-RS-Early-B-9072.jpg" 
+		  alt="Potato Image" 
+	  />
       <CardContent>
         <Button
 		  color="warning"
@@ -27,7 +32,7 @@ export default function Detector() {
           Show Results
         </Button>
         <Typography variant="body2" color="text.secondary">
-          Accuracy: 100% <br /> Disease Name: Late blight
+          Accuracy: 99% <br /> Disease Name: Late blight
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
